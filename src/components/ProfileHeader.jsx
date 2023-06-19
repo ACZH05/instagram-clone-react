a07792e27e30703dbfa6c4995267cd99ff0c1b49
Alfred Chin <alfredchin2005@gmail.com>
2023-06-19 22:13:39 +0800

    Add profile picture

--- src/components/ProfileHeader.jsx
+++ src/components/ProfileHeader.jsx
@@ -1,12 +1,14 @@
-import { Row, Col } from "react-bootstrap";
+import { Row, Col, Image } from "react-bootstrap";
 
 function ProfileHeader() {
   return (
-    <Row>
-      <Col md={3}>Picture</Col>
+    <Row className="p-5">
+      <Col md={3} className="d-flex justify-content-center align-items-center">
+        <Image src="https://sig1.co/logo-1" style={{ height: "150px"}} roundedCircle />
+      </Col>
       <Col md={9}>Des</Col>
     </Row>
   )
 }
 
-export default ProfileHeader
+export default ProfileHeader    
